d17bb74318acf5521d090937c7469123e1862512
bamjoe1 <bam.joe99@gmail.com>
2022-08-31 16:28:12 +0100

    Add files via upload
    Savgol filter removed. Uses melodic spectrogram. Finds peaks with a noise gate of 10dB

--- extract-freqs.py
+++ extract-freqs.py
@@ -34,6 +34,7 @@ from tools import *
 
 file1 = 'data/matilda-melspec.csv'
 spec = pd.read_csv(file1).to_numpy()
+spec = np.delete(spec, 0, axis=1)
 spec_db = lb.amplitude_to_db(np.abs(spec), ref=np.max(spec))
 abs_spec, ax = plt.subplots(figsize=(15,10))
 specplot = lb.display.specshow(spec_db,x_axis='time',y_axis='linear',ax=ax)
@@ -43,37 +44,38 @@ abs_spec.colorbar(specplot, ax=ax)
 
 #spectrum with max value = 961
 
-subsamples = np.arange(0, len(spec_db[0]), 100)
-savgols = np.zeros_like(spec_db)
+subsamples = np.arange(0, len(spec_db[0]), 1)
+# savgols = np.zeros_like(spec_db)
 sps = {}
 pkp = {}
 i=0
 for sample in subsamples:
 
     sp = spec_db[:,sample]
-    if len(sp)%2 == 1:
-        winlen = int(len(sp))
-    else:
-        winlen = int(len(sp) - 1)
-    savgol = sg.savgol_filter(sp,winlen,9)
+    # if len(sp)%2 == 1:
+    #     winlen = int(len(sp))
+    # else:
+    #     winlen = int(len(sp) - 1)
+    # savgol = sg.savgol_filter(sp,winlen,5)
     # bodge to fix mid-window filter spike
-    midwin = len(sp)//2
-    savgol[midwin] = 0.5*(savgol[midwin - 1]+savgol[midwin])
-    peaks = sg.find_peaks(sp,prominence=20)[0]
+    # midwin = len(sp)//2
+    # savgol[midwin] = 0.5*(savgol[midwin - 1]+savgol[midwin])
+    # savgol[255] = 0.5*(savgol[254] + savgol[256])
+    peaks = sg.find_peaks(sp, prominence=10)[0]
     peakvals = sp[peaks]
     
-    peakpowers =[]
-    strongpeaks = []
-    for peak in peaks:
-        if sp[peak] - savgol[peak] >= 20:
-            strongpeaks.append(peak)
-            peakpowers.append(sp[peak])
+    # peakpowers = []
+    # strongpeaks = []
+    # for peak in peaks:
+    #     if sp[peak] - savgol[peak] >= 20:
+    #         strongpeaks.append(peak)
+    #         peakpowers.append(sp[peak])
             
-    savgols[:,i] = savgol
+    # savgols[:,i] = savgol
     # check if list not empty and if so dump into dict
-    if strongpeaks:     
-        sps[i] = strongpeaks
-        pkp[i] = peakpowers
+    if peaks.tolist():
+        sps[i] = peaks
+        pkp[i] = peakvals
     i+=1
 
 # build array of 3-vectors for strong peaks
@@ -89,7 +91,7 @@ sp = spec_db[:,s]
 
 specfig = plt.figure(figsize=(12,8))
 plt.plot(sp, label='Spectrum')
-plt.plot(savgols[:,s], label='Savgol filter')
+# plt.plot(savgols[:,s], label='Savgol filter')
 plt.legend(loc='best')
 plt.xlabel('Freq / Hz')
 plt.ylabel('dB')
@@ -97,15 +99,15 @@ plt.scatter(sps[s], sp[sps[s]],c='r')
 
 #%% 3D SAVGOL PLOT
 
-fig = plt.figure(figsize=(15,10))
-ax = fig.add_subplot(111, projection='3d')
-Z = savgols
-y = np.linspace(0,len(spec[:,0]),len(spec[:,0]))
-x = np.linspace(1,len(spec[0]),len(spec[0]))
-X, Y = np.meshgrid(x,y)
-ax.plot_surface(X,Y,Z,label='Savgol surface')
-ax.scatter(strongpoints[:,0],strongpoints[:,1],strongpoints[:,2],c='r',label='Strong freqs')
-ax.set_ylabel('Freq / Hz')
-ax.set_xlabel('Time sample (/10)')
-ax.set_zlabel('Power / dB')
-# ax.legend(loc='best')
+# fig = plt.figure(figsize=(15,10))
+# ax = fig.add_subplot(111, projection='3d')
+# Z = savgols
+# y = np.linspace(0,len(spec[:,0]),len(spec[:,0]))
+# x = np.linspace(1,len(spec[0]),len(spec[0]))
+# X, Y = np.meshgrid(x,y)
+# ax.plot_surface(X,Y,Z,label='Savgol surface')
+# ax.scatter(strongpoints[:,0],strongpoints[:,1],strongpoints[:,2],c='r',label='Strong freqs')
+# ax.set_ylabel('Freq / Hz')
+# ax.set_xlabel('Time sample (/10)')
+# ax.set_zlabel('Power / dB')
+# # ax.legend(loc='best')
