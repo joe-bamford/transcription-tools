36b7b5612e94aa13f58f354e82080dba482bc671
Joe Bamford <bam.joe99@gmail.com>
2022-09-26 18:21:46 +0100

    Add files via upload
    Finally produces a properly calibrated spectogram with fft_note type.

--- read-data.py
+++ read-data.py
@@ -53,24 +53,55 @@ ax.legend(loc='best')
 
 raw = y_harm
 
-#%% READ IN NOTE LIBRARY FROM TXT
+# #%% READ IN NOTE LIBRARY FROM TXT
 
-notelib = pd.read_csv('note-lib.txt', sep="\s", header=None)
-notelib.columns = ["Note", "Freq", "fmin", "fmax"]
-# notelib = notelib.iloc[9:97]
-frdown, frup = notelib.iloc[0]['fmin'], notelib.iloc[-1]['fmax']
+# notelib = pd.read_csv('note-lib.txt', sep="\s", header=None)
+# notelib.columns = ["Note", "Freq", "fmin", "fmax"]
+# # notelib = notelib.iloc[9:97]
+# frdown, frup = notelib.iloc[0]['fmin'], notelib.iloc[-1]['fmax']
 
-#%% COMPUTE MELODIC SPECTROGRAM AND SAVE TO CSV
+# #%% COMPUTE MELODIC SPECTROGRAM AND SAVE TO CSV
 
-melspec = lb.feature.melspectrogram(y=raw, sr=sr, hop_length=sr//fft_sr, power=2, fmax=sr/2, n_mels=108)
-melspecframe = pd.DataFrame(melspec)
-melspecframe.to_csv('data/'+str(filename)+'-melspec.csv')
+# melspec = lb.feature.melspectrogram(y=raw, sr=sr, hop_length=sr//fft_sr, power=2, fmax=sr/2, n_mels=108)
+# melspecframe = pd.DataFrame(melspec)
+# melspecframe.to_csv('data/'+str(filename)+'-melspec.csv')
 
-#%% COMPUTE CHROMA STFT AND SAVE TO CSV
+# #%% COMPUTE CHROMA STFT AND SAVE TO CSV
 
-S = np.abs(lb.stft(y=raw, n_fft=2048))**2
-chroma = lb.feature.chroma_stft(S=S, sr=sr, hop_length=sr//fft_sr, n_chroma=12)
-chromaframe = pd.DataFrame(chroma)
-chromaframe.to_csv('data/'+str(filename)+'-chroma.csv')
+# S = np.abs(lb.stft(y=raw, n_fft=2048))**2
+# chroma = lb.feature.chroma_stft(S=S, sr=sr, hop_length=sr//fft_sr, n_chroma=12)
+# chromaframe = pd.DataFrame(chroma)
+# chromaframe.to_csv('data/'+str(filename)+'-chroma.csv')
+
+
+#%%
+
+fig, ax = plt.subplots(nrows=1, ncols=1, sharex=False)
+
+# D = lb.amplitude_to_db(np.abs(lb.stft(y)), ref=np.max)
+# D = lb.feature.melspectrogram(y=raw, sr=sr, fmax=sr/2, power=2)
+
+
+# img = lb.display.specshow(D, y_axis='mel', x_axis='time',
+
+#                                sr=sr, ax=ax[0], fmax=sr/2)
+
+# ax[0].set(title='Linear-frequency power spectrogram')
+
+# ax[0].label_outer()
+
+D = lb.amplitude_to_db(np.abs(lb.stft(raw, hop_length=sr//20)),
+
+                            ref=np.max)
+
+img = lb.display.specshow(D, y_axis='fft_note', sr=sr, hop_length=sr//20,
+
+                         x_axis='time', ax=ax)
+
+ax.set(title='Log-frequency power spectrogram')
+
+ax.label_outer()
+
+fig.colorbar(img, ax=ax, format="%+2.f dB")
 
 
